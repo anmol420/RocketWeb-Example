c1be35d088434a31b13571026e72c8962a9fb9a3
anmol420 <anmolanand2k15@gmail.com>
2024-02-24 01:42:45 +0530

    Added Sample CRUD Endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,43 @@
 #[macro_use] extern crate rocket;
 
 use rocket::serde::json::{ Value, json };
+use rocket::response::status;
 
-#[get("/")]
-fn hello() -> Value {
-    json!("Hello World!")
+#[get("/rustaceans")]
+fn get_rustaceans() -> Value {
+    json!([{"id":1, "name":"John Doe"}, {"id":2, "name":"Johnson Doe"}])
+}
+
+#[get("/rustaceans/<id>")]
+fn view_rustacean(id: i32) -> Value {
+    json!({"id":id, "name":"Anmol Anand", "email":"aa@gmail.com"})
+}
+
+#[post("/rustaceans", format="json")]
+fn create_rustacean() -> Value {
+    json!({"id":3, "name":"Anmol Anand", "email":"aa@gmail.com"})
+}
+
+#[put("/rustaceans/<id>", format="json")]
+fn update_rustacean(id: i32) -> Value {
+    json!({"id":id, "name":"Anmol Anand", "email":"aa@gmail.com"})
+}
+
+#[delete("/rustaceans/<_id>")]
+fn delete_rustacean(_id: i32) -> status::NoContent {
+    status::NoContent
 }
 
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
-        .mount("/", routes![hello])
+        .mount("/", routes![
+            get_rustaceans,
+            view_rustacean,
+            create_rustacean,
+            update_rustacean,
+            delete_rustacean
+        ])
         .launch()
         .await;
 }
