5e405148ca1c9a80be4a7bcfc771c64c1457dee4
anmol420 <anmolanand2k15@gmail.com>
2024-03-01 12:53:34 +0530

    Added GET, POST, PUT And DELETE Request

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod schema;
 mod models;
 
 use diesel::prelude::*;
+use rocket::execute;
 use auth::BasicAuth;
 use rocket::serde::json::{Value, json, Json};
 use rocket::response::status;
@@ -19,14 +20,23 @@ struct DbConn(SqliteConnection);
 #[get("/rustaceans")]
 async fn get_rustaceans(_auth: BasicAuth, db: DbConn) -> Value {
     db.run(|c| {
-        let rustaceans = rustaceans::table.order(rustaceans::id.desc()).limit(1000).load::<Rustacean>(c).expect("DB error");
+        let rustaceans = rustaceans::table
+            .order(rustaceans::id.asc())
+            .limit(1000)
+            .load::<Rustacean>(c)
+            .expect("Database error");
         json!(rustaceans)
     }).await
 }
 
 #[get("/rustaceans/<id>")]
-fn view_rustacean(id: i32, _auth: BasicAuth) -> Value {
-    json!({"id": id, "name": "John Doe", "email": "john@doe.com"})
+async fn view_rustacean(id: i32, _auth: BasicAuth, db: DbConn) -> Value {
+    db.run(move |c| {
+        let rustacean = rustaceans::table.find(id)
+            .get_result::<Rustacean>(c)
+            .expect("Failed To Search");
+        json!(rustacean)
+    }).await
 }
 
 #[post("/rustaceans", format = "json", data = "<new_rustacean>")]
@@ -35,29 +45,43 @@ async fn create_rustacean(_auth: BasicAuth, db: DbConn, new_rustacean: Json<NewR
         let result = diesel::insert_into(rustaceans::table)
             .values(new_rustacean.into_inner())
             .execute(c)
-            .expect("Failed To Add !");
+            .expect("Failed To Add");
         json!(result)
     }).await
 }
 
-#[put("/rustaceans/<id>", format = "json")]
-fn update_rustacean(id: i32, _auth: BasicAuth) -> Value {
-    json!({"id": id, "name": "John Doe", "email": "john@doe.com"})
+#[put("/rustaceans/<id>", format = "json", data = "<rustacean>")]
+async fn update_rustacean(id: i32, _auth: BasicAuth, db: DbConn, rustacean: Json<NewRustacean>) -> Value {
+    db.run(move |c| {
+        let result = diesel::update(rustaceans::table.find(id))
+            .set((
+                rustaceans::email.eq(rustacean.email.to_owned()),
+                rustaceans::name.eq(rustacean.name.to_owned())
+            ))
+            .execute(c)
+            .expect("Failed To Update");
+        json!(result)
+    }).await
 }
 
-#[delete("/rustaceans/<_id>")]
-fn delete_rustacean(_id: i32, _auth: BasicAuth) -> status::NoContent {
-    status::NoContent
+#[delete("/rustaceans/<id>")]
+async fn delete_rustacean(id: i32, _auth: BasicAuth, db: DbConn) -> status::NoContent {
+    db.run(move |c| {
+        diesel::delete(rustaceans::table.find(id))
+            .execute(c)
+            .expect("Failed To Delete");
+        status::NoContent
+    }).await
 }
 
 #[catch(404)]
 fn not_found() -> Value {
-    json!("Not found!")
+    json!("Not found")
 }
 
 #[catch(422)]
 fn not_processible() -> Value {
-    json!("Some Entry Field(s) Are Missing!")
+    json!("Some Entry Field(s) Are Missing")
 }
 
 #[rocket::main]
